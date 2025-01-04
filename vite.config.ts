679466ab6d5984dfd9dd7c08ecc13aab327f8d39
Iain Davis <iain@iaindavis.dev>
2025-01-04 14:12:12 -0800

    align Vite's path configuration with that from TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
+        tsconfigPaths(),
         react(),
     ],
     css: {
         modules: {
             scopeBehaviour: 'local',
             generateScopedName: '[name]__[local]__[hash:base64:5]'
-        }
-    },
-    resolve: {
-        alias: {
-            '@components': path.resolve(__dirname, 'src/iaindavis-dev/docusaurus-extensions/components'),
-            '@dummies': path.resolve(__dirname, 'src/dummies')
         }
     },
 })
