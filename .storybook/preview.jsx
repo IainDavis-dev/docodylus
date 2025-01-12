beabfe9e49e816a10e330e110d136cd1c9be1c52
Iain Davis <iain@iaindavis.dev>
2025-01-12 15:01:36 -0800

    add locale picker to Storybook preview environment

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,25 @@
 /** @type { import('@storybook/react').Preview } */
+const supportedLocales = ['en', 'es'];
+
 const preview = {
+  initialGlobals: {
+    locale: 'en', // default
+  },
+  globalTypes: {
+    locale: {
+      name: 'Locale',
+      description: 'Internationalization locale',
+      defaultValue: 'en',
+      toolbar: {
+        icon: 'globe',
+        items: supportedLocales.map((locale) => ({
+          value: locale,
+          title: locale.toUpperCase(),
+        })),
+        dynamicTitle: true
+      }
+    }
+  },
   parameters: {
     controls: {
       matchers: {
