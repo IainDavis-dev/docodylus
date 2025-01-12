1cf4024a21443a94e385bc4fcd5da4b10fe76367
Iain Davis <iain@iaindavis.dev>
2025-01-12 14:58:11 -0800

    update storybook configuration
    * add path specs for files with extension`.docs.mdx`
    * enable GitHub-flavored markdown and Gitmoji in docs (remark plugins)
    * add path aliases for '@components', '@dummies', '@docs', '@validation' and '@i18n'

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,29 @@
+import remarkGfm from 'remark-gfm';
+import remarkGitmoji from 'remark-gemoji';
+import path from 'path';
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)",
+    "../docs/**/*.docs.mdx",
+    "../src/**/*.docs.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-docs",
+    "@storybook/addon-links",
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm, remarkGitmoji]
+          }
+        }
+
+      }
+    }
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -14,6 +31,18 @@ const config = {
   },
   core: {
     builder: '@storybook/builder-vite'
+  },
+  viteFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@dummies': path.resolve(__dirname, '../src/dummies'),
+      '@i18n': path.resolve(__dirname, '../src/infra/i18n'),
+      '@validation': path.resolve(__dirname, '../src/infra/validation'),
+      '@docs': path.resolve(__dirname, '../docs'),
+    }
+
+    return config;
   }
 };
 export default config;
