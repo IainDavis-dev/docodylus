ed19afec0da25b41143fc8e4d516ed0f0e371977
Iain Davis <iain@iaindavis.dev>
2025-02-22 10:46:39 -0800

    update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
-        tsconfigPaths(),
         react(),
     ],
     css: {
         modules: {
             scopeBehaviour: 'local',
             generateScopedName: '[name]__[local]__[hash:base64:5]'
+        }
+    },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, './src/components'),
+            '@dummies': path.resolve(__dirname, './src/dummies'),
+            '@i18n': path.resolve(__dirname, './src/infra/i18n'),
+            '@validation': path.resolve(__dirname, './src/infra/validation'),
+            '@docs': path.resolve(__dirname, './docs'),
+            '@test-utils': path.resolve(__dirname, './test/test-utils'),
+            '@types': path.resolve(__dirname, './src/types'),
+            '@shared-utils': path.resolve(__dirname, './src/infra/sharedUtils'),
         }
     },
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,54 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, UserConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './vitest.setup.ts',
-        include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}']
-    }
-})
+export default defineConfig(async () => {
+    const { default: mdxPlugin } = await (import('@mdx-js/rollup'))
+
+    const config = {
+        ...viteConfig,
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './vitest.setup.ts',
+            include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}', 'test/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+            root: ".",
+            coverage: {
+                provider: 'istanbul',
+                reporter: ['html', 'text', 'lcov'],
+                reportsDirectory: 'static/reports/coverage',
+                all: true,
+                thresholds: {
+                    statements: 20,
+                    branches: 20,
+                    functions: 20,
+                    lines: 20,
+                },
+                exclude: [
+                    "build", 
+                    "static",
+                    ".*",   // no dot-files
+
+                    // typescript and javascript files in the root directory tend to be config files
+                    "*.ts",
+                    "*.js", 
+
+                    // I think I want to avoid testing all the markdown files. May revisit later
+                    // May just do snapshot tests on the rendered content later so at least I can detect if something breaks unexpectedly.
+                    "*.mdx",
+                    "*.md",
+                    "**/*.test.*",
+                    "**/*.spec.*",
+                    "**/*.stories.*",
+                ]
+            } as any // something of a desperation move... VS Code is complaining about the perfectly-valid `provider: 'istanbul'` property. Note: everything is fine at runtime, it's just a TS check throwing a spurious warning.
+        },
+        plugins: [
+            ...(viteConfig.plugins || []),
+            mdxPlugin()
+        ],
+    };
+
+    console.log("Test configs:", JSON.stringify(config.test, null, 2))
+
+    return config;
+});
