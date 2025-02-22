ec3a2792723125073a1c4edc1bd4cec5d75be952
Iain Davis <iain@iaindavis.dev>
2025-02-22 10:47:52 -0800

    add generate-valid-locales script

---
+++ scripts/generate-valid-locales.mjs
@@ -0,0 +1,14 @@
+import { writeFile } from "fs/promises";
+import { all as localeCodes } from 'locale-codes';
+
+const validLocales = localeCodes.map(({ tag }) => tag).sort((a, b) => a < b ? -1 : 1)
+const output = `
+// This file is auto-generated at build time. Do not edit manually.
+
+const validLocales = ${JSON.stringify(validLocales, null, 2)} as const;
+
+export validLocales;
+export type ValidLocale = typeof validLocales[number];
+`
+
+await writeFile("src/infra/i18n/types/validLocales.ts", output)
