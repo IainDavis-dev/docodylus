318149d787190886c6e6f5f9f55fb9976348c202
Iain Davis <iain@iaindavis.dev>
2025-02-28 15:04:10 -0800

    rework i18n consts

--- src/infra/i18n/consts.ts
+++ src/infra/i18n/consts.ts
@@ -1,20 +1,38 @@
+import { Namespaced } from "@shared-utils/types";
 import { LocalizedStrings, ValidLocale } from "./types";
+import { createNamespacePrepender } from "@shared-utils/createNamespacePrepender";
 
 export const BASE_NAMESPACE = "dev.iaindavis.docodylus";
 export const DEFAULT_LOCALE: ValidLocale = "en";
 
-export const SUPPORTED_LOCALES = [
+export const SUPPORTED_LOCALES: ValidLocale[] = [
     "en",
     "es"
-];
+] as const;
+
 
 /**
  * Set of translations supported without explicitly being imported into LocaleAwarePolglot
  * 
  * Note: translated messages primarily indicate the status of import operations for other translations
  */
-export const TXLNS_LOADING_KEY: string = "dev.iaindavis.docodylus.i18n.txlns-loading"
-export const DEFAULT_TRANSLATIONS: Partial<Record<ValidLocale, LocalizedStrings>> = {
-    ['en']: { [TXLNS_LOADING_KEY]:  "translations loading..." },
-    ['es']: { [TXLNS_LOADING_KEY]: "traducciones cargándose..." }
+const I18nNamespace = "dev.iaindavis.docodylus.internationalization" as const;
+const prependNamespace = createNamespacePrepender(I18nNamespace);
+
+
+type DefaultLocalizedStrings = Namespaced<typeof I18nNamespace, {
+    "txlns-loading": string;
+}>
+
+export const DEFAULT_TRANSLATIONS: Partial<Record<ValidLocale, DefaultLocalizedStrings>> = {
+    ['en']: prependNamespace({ ["txlns-loading"]:  "translations loading..." }),
+    ['es']: prependNamespace({ ["txlns-loading"]: "traducciones cargándose..." })
 } as const
+
+export const TXLNS_LOADING_KEY: keyof DefaultLocalizedStrings = "dev.iaindavis.docodylus.internationalization.txlns-loading" as const;
+
+// adds localized-string keys for this component to the master list so
+// it can be included in user overrides.
+declare module "@i18n/types" {
+    export interface DocodylusLocalizedStrings extends DefaultLocalizedStrings {}
+}
