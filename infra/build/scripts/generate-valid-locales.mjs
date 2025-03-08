f197233bcc51e4626ad8422d5bdcbd78791910f2
Iain Davis <iain@iaindavis.dev>
2025-03-08 14:26:01 -0800

    move build script into new  folder

---
+++ infra/build/scripts/generate-valid-locales.mjs
@@ -0,0 +1,14 @@
+import { writeFile } from 'fs/promises';
+import { all as localeCodes } from 'locale-codes';
+
+const validLocales = localeCodes.map(({ tag }) => tag).sort((a, b) => (a < b ? -1 : 1));
+const output = `/* eslint-disable comma-dangle */
+// This file is auto-generated at build time. Do not edit manually.
+
+const validLocales = ${JSON.stringify(validLocales, null, 2).replaceAll('"', '\'')} as const;
+
+export { validLocales };
+export type ValidLocale = typeof validLocales[number];
+`;
+
+await writeFile('src/common/i18n/types/validLocales.ts', output);
