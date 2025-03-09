a809b367b388a43ae5dbe95f264827a3ada0f9f4
Iain Davis <iain@iaindavis.dev>
2025-03-09 14:19:44 -0700

    fix(infra): support dynamic location of 'package.json' files for  modules

--- infra/build/release/semantic-release.config.js
+++ infra/build/release/semantic-release.config.js
@@ -1,3 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { glob } from 'glob';
+
+const getPackageJsonDirectories = () => {
+  const filePaths = [
+    '.', // root package.json path
+    ...glob
+      .sync('src/**/package.json', { absolute: false })
+      .map((filePath) => filePath.replace('/package.json', '')),
+  ];
+
+  return [...filePaths];
+};
+
+const getNpmPluginConfigs = () => {
+  const npmPluginConfigs = getPackageJsonDirectories().map((dirPath) => [
+    '@semantic-release/npm',
+    { npmPublish: false, pkgRoot: dirPath },
+  ]);
+  console.log(npmPluginConfigs);
+
+  return npmPluginConfigs;
+};
+
 export default {
   branches: [
     'main',
@@ -7,12 +31,7 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
+    ...getNpmPluginConfigs(),
     [
       '@semantic-release/git',
       {
