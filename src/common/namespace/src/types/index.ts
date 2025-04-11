0cebd48687ec945e2ec681bc36fccd21ed2c5ba2
Iain Davis <iain@iaindavis.dev>
2025-04-10 17:56:06 -0700

    refactor(namespace): move ValidNamespace into it's own file and re-export from the types index file

--- src/common/namespace/src/types/index.ts
+++ src/common/namespace/src/types/index.ts
@@ -1,23 +1,2 @@
-import { IsLowercase } from '@validation/types';
-
 export { Namespaced } from './namespace';
-
-type IsValidNamespace<T extends string> =
-  T extends ''
-    // Reject empty string outright.
-    ? false
-    // segmented namespace
-    : T extends `${infer Segment}.${infer Rest}`
-        ? Segment extends ''
-            // Reject empty segment (leading or double dots)
-            ? false
-            : IsLowercase<Segment> extends true
-                // Recurse on the rest.
-                ? IsValidNamespace<Rest>
-                // reject if contains disallowed characters
-                : false
-            : IsLowercase<T> extends true
-                ? true
-                : false;
-
-export type ValidNamespace<T extends string> = IsValidNamespace<T> extends true ? T : never;
+export { ValidNamespace } from './ValidNamespace'
