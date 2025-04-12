763bf86d958801f49cd8ddcfb6386fb1e95607ec
Iain Davis <iain@iaindavis.dev>
2025-04-11 17:41:31 -0700

    refactor(i18n): add public interface for i18n module

---
+++ src/common/i18n/src/index.ts
@@ -0,0 +1,3 @@
+export { I18nProvider } from '@i18n/context/I18nProvider';
+export { useTranslations } from '@i18n/hooks/useTranslations';
+export { DocodylusLocalizedStrings } from '@i18n/types/localizedStrings';
