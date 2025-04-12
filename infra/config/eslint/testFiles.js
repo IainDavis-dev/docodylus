a6ddd9b84cbc237249308051284eb47924e4f87e
Iain Davis <iain@iaindavis.dev>
2025-04-12 11:48:22 -0700

    chore(core): set eslint project to test-specific tsconfig file for test files

--- infra/config/eslint/testFiles.js
+++ infra/config/eslint/testFiles.js
@@ -4,6 +4,9 @@ import { TEST_FILES } from './consts.js';
 /** @type {import('eslint').Linter.Config} */
 export const testFilesConfig = {
   files: TEST_FILES,
+  parserOptions: {
+    project: './tsconfig.test.json'
+  },
   languageOptions: {
     globals: {
       ...globals.vitest,
