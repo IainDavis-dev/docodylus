7a36ec5244ecf00ffaefb40aea52d2bf4d0843d2
Iain Davis <iain@iaindavis.dev>
2025-04-12 15:32:35 -0700

    chore(infra): fix output path of generate-valid-locales script to reflect refactored folder

--- infra/build/scripts/generate-valid-locales.mjs
+++ infra/build/scripts/generate-valid-locales.mjs
@@ -11,4 +11,4 @@ export { validLocales };
 export type ValidLocale = typeof validLocales[number];
 `;
 
-await writeFile('src/common/i18n/types/validLocales.ts', output);
+await writeFile('src/common/i18n/src/types/validLocales.ts', output);
