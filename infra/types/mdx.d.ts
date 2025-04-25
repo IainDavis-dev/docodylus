f003ee5f86b00aa59fcd30a3f9a3be188594bbe6
Iain Davis <iain@iaindavis.dev>
2025-04-25 09:25:10 -0700

    chore: move test-scoped declaration file for MDX modules into **infra** folder

--- infra/types/mdx.d.ts
+++ infra/types/mdx.d.ts
@@ -1,7 +1,7 @@
-// allow import of MDX files as React (JSX) components
 declare module '*.mdx' {
     import { JSX } from 'react';
 
     const MDXComponent: (props: unknown) => JSX.Element;
     export default MDXComponent;
 }
+  
