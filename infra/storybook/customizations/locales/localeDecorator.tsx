3623dcd811315f417e3951a7ef5f3358cc984ddb
Iain Davis <iain@iaindavis.dev>
2025-04-25 10:06:23 -0700

    chore: update imports

--- infra/storybook/customizations/locales/localeDecorator.tsx
+++ infra/storybook/customizations/locales/localeDecorator.tsx
@@ -1,6 +1,6 @@
 import { JSX } from 'react';
 import { StoryContext } from '@storybook/react';
-import { I18nProvider } from '@i18n/context/I18nProvider';
+import { I18nProvider } from '@i18n';
 
 const localeDecorator = (Story: () => JSX.Element, context: StoryContext) => (
     <I18nProvider locale={context.globals.locale} >
