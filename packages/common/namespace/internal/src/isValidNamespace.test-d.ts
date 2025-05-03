31050696967a8e057f540ce75531a070c8916f09
Iain Davis <iain@iaindavis.dev>
2025-05-03 08:47:45 -0700

    chore: fix some test cases that were previously edited, I guess?

--- packages/common/namespace/internal/src/isValidNamespace.test-d.ts
+++ packages/common/namespace/internal/src/isValidNamespace.test-d.ts
@@ -9,7 +9,7 @@ describeTypeTest('type ValidNamespace', () => {
     });
 
     it('should accept a string of all lowercase alpha', () => {
-      expectTypeOf<ValidNamespace<'abcde.fghij.klmnop'>>().not.toBeNever();
+      expectTypeOf<ValidNamespace<'abcde'>>().not.toBeNever();
     });
 
     it('should accept a string of all alpha lowercase alpha segments delimited by single dots', () => {
@@ -18,7 +18,7 @@ describeTypeTest('type ValidNamespace', () => {
 
     it('should accept a single-letter namespace', () => {
       // but don't do this.
-      expectTypeOf<ValidNamespace<'abcde.fghij.klmnop'>>().not.toBeNever();
+      expectTypeOf<ValidNamespace<'k'>>().not.toBeNever();
     });
   });
 
